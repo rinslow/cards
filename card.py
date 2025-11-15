3377ae3fdc6359903d9dc554d637a1fa5c8ae042
Dan Elkis <redacted@redact.ed>
2025-11-15 23:34:41 +0200

    card

--- card.py
+++ card.py
@@ -1,5 +1,6 @@
 import random
 from enum import Enum
+from typing import Iterator
 
 class Suit(Enum):
   HEARTS = "hearts"
@@ -66,6 +67,25 @@ class Game:
 class Player:
   pass
 
+class Hand:
+  """A hand is a collection of cards."""
+  def __init__(self):
+    self.cards = []
+
+  def add_card(self, card: Card) -> None:
+    self.cards.append(card)
+
+  def __iter__(self) -> Iterator[Card]:
+    return iter(self.cards)
+
+  def __len__(self) -> int:
+    return len(self.cards)
+
+  def __str__(self):
+    if len(self) == 0:
+      return "<Empty hand>"
+    return ", ".join([str(card) for card in self.cards])
+
 class Deck:
   def __init__(self, shuffled: bool = True):
     self.cards = [Card(suit, rank) for suit in Suit for rank in Rank]
@@ -76,8 +96,11 @@ class Deck:
   def shuffle(self) -> None:
     random.shuffle(self.cards)
 
-  def draw(self) -> Card:
+  def draw_card(self) -> Card:
     return self.cards.pop()
+
+  def draw_cards(self, count: int = 1) -> list[Card]:
+    return [self.cards.pop() for _ in range(count)]
 
   def __str__(self):
     card_strs = [str(card) for card in self.cards]
@@ -88,5 +111,21 @@ class Deck:
     return ",\n".join(lines)
 
 if __name__ == "__main__":
-    deck = Deck()
-    print(deck)
+    # deck = Deck()
+    # player1_5_cards = deck.draw_cards(5)
+    # player2_5_cards = deck.draw_cards(5)
+    # print(", ".join([str(x) for x in player1_5_cards]))
+    # print(", ".join([str(x) for x in player2_5_cards]))
+    # print("-" * 40)
+    # print(deck)
+
+    hand = Hand()
+    hand.add_card(Card(Suit.HEARTS, Rank.ACE))
+    hand.add_card(Card(Suit.HEARTS, Rank.KING))
+    hand.add_card(Card(Suit.HEARTS, Rank.QUEEN))
+    hand.add_card(Card(Suit.HEARTS, Rank.JACK))
+    hand.add_card(Card(Suit.HEARTS, Rank.TEN))
+    print(hand)
+
+    for card in hand:
+      print(card)
